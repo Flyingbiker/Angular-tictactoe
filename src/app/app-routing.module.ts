0efa7a2b739919186f5c103b70d92b164c9084be
Flyingbiker <schoebel.aymeric@gmail.com>
2021-01-28 09:26:33 +0100

    initialisation du projet et création des 2 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { ParamPageComponent } from './param-page/param-page.component';
+import { GamePageComponent } from './game-page/game-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/', component GamePageComponent,
+    path: '/params', component ParamPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
